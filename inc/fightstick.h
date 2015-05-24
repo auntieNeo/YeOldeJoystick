19855a3af28cc0f6f8da7d21dc30d41fc0d7fa35
Jonathan Glines <auntieNeo@gmail.com>
2015-05-23 19:37:12 -0600

    Working with Teensy++ 2.0

--- inc/fightstick.h
+++ inc/fightstick.h
@@ -39,52 +39,149 @@
  * fightstick button masks and defines
  */
 
-uint8_t BUTTON = 0;
-uint8_t STICK = 0;
-uint8_t EXTRA = 0;
+uint8_t PORT_A_INPUT = 0;
+uint8_t PORT_B_INPUT = 0;
+uint8_t PORT_C_INPUT = 0;
+uint8_t PORT_D_INPUT = 0;
+uint8_t PORT_E_INPUT = 0;
+uint8_t PORT_F_INPUT = 0;
 
-#define BUTTON_CIRCLE   (1 << 0)
-#define BUTTON_CROSS    (1 << 1)
-#define BUTTON_TRIANGLE (1 << 2)
-#define BUTTON_SQUARE   (1 << 3)
-#define BUTTON_R1       (1 << 4)
-#define BUTTON_R2       (1 << 5)
-#define BUTTON_L1       (1 << 6)
-#define BUTTON_L2       (1 << 7)
+#define _port(input)  PORT_ ## input
+#define _pin(input)  PIN_ ## input
+#define _input(port)  PORT_ ## port ## _INPUT
 
-#define STICK_UP         (1 << 0)
-#define STICK_DOWN       (1 << 1)
-#define STICK_LEFT       (1 << 4)
-#define STICK_RIGHT      (1 << 5)
+#define TEENSY_2_0_PLUS_PLUS
 
-#define EXTRA_START    (1 << 0)
-#define EXTRA_SELECT   (1 << 1)
-#define EXTRA_PS      (1 << 2)
-#define EXTRA_REBOOT  (1 << 3)
+
+#ifdef TEENSY_1_0
+#define PORT_CIRCLE C
+#define PIN_CIRCLE 2
+
+#define PORT_CROSS D
+#define PIN_CROSS 0
+
+#define PORT_TRIANGLE D
+#define PIN_TRIANGLE 1
+
+#define PORT_SQUARE D
+#define PIN_SQUARE 2
+
+#define PORT_R1 B
+#define PIN_R1 3
+
+#define PORT_R2 B
+#define PIN_R2 4
+
+#define PORT_L1 B
+#define PIN_L1 5
+
+#define PORT_L2 D
+#define PIN_L2 3
+
+#define PORT_UP C
+#define PIN_UP 4
+
+#define PORT_DOWN C
+#define PIN_DOWN 5
+
+#define PORT_LEFT C
+#define PIN_LEFT 6
+
+#define PORT_RIGHT C
+#define PIN_RIGHT 7
+
+#define PORT_START D
+#define PIN_START 4
+
+#define PORT_SELECT D
+#define PIN_SELECT 5
+
+#define PORT_PS D
+#define PIN_PS 7
+
+#define LED_CONFIG	(DDRD |= (1<<6))
+#define LED_ON	(PORTD &= ~(1<<6))
+#define LED_OFF	(PORTD |= (1<<6))
+#endif
+
+#ifdef TEENSY_2_0_PLUS_PLUS
+#define PORT_CIRCLE B
+#define PIN_CIRCLE 7
+
+#define PORT_CROSS D
+#define PIN_CROSS 0
+
+#define PORT_TRIANGLE D
+#define PIN_TRIANGLE 1
+
+#define PORT_SQUARE D
+#define PIN_SQUARE 2
+
+#define PORT_R1 E
+#define PIN_R1 6
+
+#define PORT_R2 E
+#define PIN_R2 7
+
+#define PORT_L1 B
+#define PIN_L1 0
+
+#define PORT_L2 D
+#define PIN_L2 3
+
+#define PORT_UP B
+#define PIN_UP 6
+
+#define PORT_DOWN B
+#define PIN_DOWN 5
+
+#define PORT_LEFT B
+#define PIN_LEFT 4
+
+#define PORT_RIGHT B
+#define PIN_RIGHT 3
+
+#define PORT_START D
+#define PIN_START 4
+
+#define PORT_SELECT D
+#define PIN_SELECT 5
+
+#define PORT_PS D
+#define PIN_PS 7
+
+#define LED_CONFIG	(DDRD |= (1<<6))
+#define LED_OFF	(PORTD &= ~(1<<6))
+#define LED_ON	(PORTD |= (1<<6))
+#endif
+
 
 /**********************************************************
  * gamepad button defines
  */
 
-#define GAMEPAD_CIRCLE_ON	    (BUTTON & BUTTON_CIRCLE)
-#define GAMEPAD_CROSS_ON	    (BUTTON & BUTTON_CROSS)
-#define GAMEPAD_TRIANGLE_ON	  (BUTTON & BUTTON_TRIANGLE)
-#define GAMEPAD_SQUARE_ON	    (BUTTON & BUTTON_SQUARE)
+#define _check_input(port, pin)  _input(port) & (1 << pin)
+#define check_input(input)  _check_input(_port(input), _pin(input))
 
-#define GAMEPAD_R1_ON	        (BUTTON & BUTTON_R1)
-#define GAMEPAD_R2_ON	        (BUTTON & BUTTON_R2)
-#define GAMEPAD_L1_ON	        (BUTTON & BUTTON_L1)
-#define GAMEPAD_L2_ON	        (BUTTON & BUTTON_L2)
+#define GAMEPAD_CIRCLE_ON	    (check_input(CIRCLE))
+#define GAMEPAD_CROSS_ON	    (check_input(CROSS))
+#define GAMEPAD_TRIANGLE_ON	  (check_input(TRIANGLE))
+#define GAMEPAD_SQUARE_ON	    (check_input(SQUARE))
 
-#define GAMEPAD_UP_ON	        (STICK & STICK_UP)
-#define GAMEPAD_DOWN_ON	      (STICK & STICK_DOWN)
-#define GAMEPAD_LEFT_ON       (STICK & STICK_LEFT)
-#define GAMEPAD_RIGHT_ON	    (STICK & STICK_RIGHT)
+#define GAMEPAD_R1_ON	        (check_input(R1))
+#define GAMEPAD_R2_ON	        (check_input(R2))
+#define GAMEPAD_L1_ON	        (check_input(L1))
+#define GAMEPAD_L2_ON	        (check_input(L2))
 
-#define GAMEPAD_START_ON		  (EXTRA & EXTRA_START)
-#define GAMEPAD_SELECT_ON	    (EXTRA & EXTRA_SELECT)
-#define GAMEPAD_PS_ON	        (EXTRA & EXTRA_PS)
-#define GAMEPAD_REBOOT_ON	    (EXTRA & EXTRA_REBOOT)
+#define GAMEPAD_UP_ON	        (check_input(UP))
+#define GAMEPAD_DOWN_ON	      (check_input(DOWN))
+#define GAMEPAD_LEFT_ON       (check_input(LEFT))
+#define GAMEPAD_RIGHT_ON	    (check_input(RIGHT))
+
+#define GAMEPAD_START_ON		  (check_input(START))
+#define GAMEPAD_SELECT_ON	    (check_input(SELECT))
+#define GAMEPAD_PS_ON	        (check_input(PS))
+#define GAMEPAD_REBOOT_ON	    (0)
 
 /**********************************************************
  * fightpad pins
@@ -132,22 +229,26 @@ uint8_t EXTRA = 0;
 
 void gamepad_init(void) {
     // set input with pull-up resistors
-#define PULLUP_INPUT(port, pin)  DDR ## port &= ~pin;  PORT ## port |= pin;
-    PULLUP_INPUT(B, BUTTON_CIRCLE);
-    PULLUP_INPUT(B, BUTTON_CROSS);
-    PULLUP_INPUT(B, BUTTON_TRIANGLE);
-    PULLUP_INPUT(B, BUTTON_SQUARE);
-    PULLUP_INPUT(B, BUTTON_R1);
-    PULLUP_INPUT(B, BUTTON_R2);
-    PULLUP_INPUT(B, BUTTON_L1);
-    PULLUP_INPUT(B, BUTTON_L2);
-    PULLUP_INPUT(F, STICK_UP);
-    PULLUP_INPUT(F, STICK_DOWN);
-    PULLUP_INPUT(F, STICK_LEFT);
-    PULLUP_INPUT(F, STICK_RIGHT);
-    PULLUP_INPUT(D, EXTRA_START);
-    PULLUP_INPUT(D, EXTRA_SELECT);
-    PULLUP_INPUT(D, EXTRA_PS);
+#define __pullup_input(port, pin)  DDR ## port &= ~(1 << pin);  PORT ## port |= (1 << pin);
+#define _pullup_input(port, pin)  __pullup_input(port, pin)
+#define pullup_input(input)  _pullup_input(_port(input), _pin(input))
+    pullup_input(CIRCLE);
+    pullup_input(CROSS);
+    pullup_input(TRIANGLE);
+    pullup_input(SQUARE);
+    pullup_input(R1);
+    pullup_input(R2);
+    pullup_input(L1);
+    pullup_input(L2);
+    pullup_input(UP);
+    pullup_input(DOWN);
+    pullup_input(LEFT);
+    pullup_input(RIGHT);
+    pullup_input(START);
+    pullup_input(SELECT);
+    pullup_input(PS);
+
+    LED_CONFIG;
 }
 
 /**********************************************************
@@ -155,26 +256,38 @@ void gamepad_init(void) {
  */ 
 
 void gamepad_read(void) {
-    // read button inputs
-    BUTTON = PINB & BUTTON_CIRCLE;
-    BUTTON |= PINB & BUTTON_CROSS;
-    BUTTON |= PINB & BUTTON_TRIANGLE;
-    BUTTON |= PINB & BUTTON_SQUARE;
-    BUTTON |= PINB & BUTTON_R1;
-    BUTTON |= PINB & BUTTON_R2;
-    BUTTON |= PINB & BUTTON_L1;
-    BUTTON |= PINB & BUTTON_L2;
+#define __read_input(port, pin)  _input(port) |= ~(PIN ## port) & (1 << pin);
+#define _read_input(port, pin)  __read_input(port, pin)
+#define read_input(input)  _read_input(_port(input), _pin(input))
 
-    // read stick inputs
-    STICK = PINF & STICK_UP;
-    STICK |= PINF & STICK_DOWN;
-    STICK |= PINF & STICK_LEFT;
-    STICK |= PINF & STICK_RIGHT;
-    
-    // read extra inputs
-    EXTRA = PIND & EXTRA_START;
-    EXTRA |= PIND & EXTRA_SELECT;
-    EXTRA |= PIND & EXTRA_PS;
+    PORT_A_INPUT = 0;
+    PORT_B_INPUT = 0;
+    PORT_C_INPUT = 0;
+    PORT_D_INPUT = 0;
+    PORT_E_INPUT = 0;
+    PORT_F_INPUT = 0;
+
+    read_input(CIRCLE);
+    read_input(CROSS);
+    read_input(TRIANGLE);
+    read_input(SQUARE);
+    read_input(R1);
+    read_input(R2);
+    read_input(L1);
+    read_input(L2);
+    read_input(UP);
+    read_input(DOWN);
+    read_input(LEFT);
+    read_input(RIGHT);
+    read_input(START);
+    read_input(SELECT);
+    read_input(PS);
+
+    // feedback on LED
+    if (PORT_A_INPUT || PORT_B_INPUT || PORT_C_INPUT || PORT_D_INPUT || PORT_E_INPUT || PORT_F_INPUT)
+      LED_ON;
+    else
+      LED_OFF;
 
     // TODO: define virtual buttons for reboot
 
--- ps3_gamepad/gamepad.c
+++ ps3_gamepad/gamepad.c
@@ -58,8 +58,8 @@
 #endif
 
 #define LED_CONFIG	(DDRD |= (1<<6))
-#define LED_ON	(PORTD &= ~(1<<6))
-#define LED_OFF	(PORTD |= (1<<6))
+#define LED_OFF	(PORTD &= ~(1<<6))
+#define LED_ON	(PORTD |= (1<<6))
 
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 
